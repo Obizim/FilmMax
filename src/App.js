e7a6218be32204850cce9e82b5991ab1880f705b
Zim <obizimuzo@gmail.com>
2020-12-10 15:08:28 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import { Provider } from 'react-redux'
+// import UserContainer from './components/UserContainer'
+// import store from './components/redux/store';
+
+function App() {
+  return (
+    // <Provider store={store}>
+    <div className="App">
+      <p>Hello React</p>
+    {/* <UserContainer /> */}
+    </div>
+    // </Provider>
+  );
+}
+
+export default App;
